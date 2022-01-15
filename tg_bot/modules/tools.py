45a636b6d86a8ee15f57c0a8e93c717380478eb9
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-16 00:58:29 +0530

    Update tools.py

--- tg_bot/modules/tools.py
+++ tg_bot/modules/tools.py
@@ -1,57 +1,16 @@
-__mod_name__ = "Tᴏᴏʟꜱ"
+__mod_name__ = "STRING GEN"
 
 __help__ = """
 *Yuriko Info bot*
 
-✗  /sg <reply> - `To check history name`
+✨ Available Commands ✨
 
-*Date-time-Weather*
-
-✗  /time - `<country code> Gives information about a timezone.`
-
-✗  /weather - `<city> Get weather info in a particular place.`
-
-✗  /wttr - `<city> Advanced weather module, usage same as /weather`
-
-✗  /wttr - `moon Get the current status of moon`
-
-*Converts*
-
-✗  /encrypt - `Encrypts The Given Text`
-
-✗  /decrypt - `Decrypts Previously Ecrypted Text`
-
-✗  /zip - `reply to a telegram file to compress it in .zip format`
-
-✗  /unzip - `reply to a telegram file to decompress it from the .zip format`
-
-*Random API*
-
-✗  /ptl *or* /asupan - `get random video from Instagram timeline`
-
-✗  /chika - `get random video from chikakiku`
-
-✗  /wibu - `get random short anime video or photos`
-
-✗  /truth - `challenge`
-
-✗  /dare - `challenge`
-
-*Link To File:*
-
-✗  /up - `reply to a direct download link to upload it to telegram as files`
- 
- *File To Link:*
-
-✗  /transfersh - `reply to a telegram file to upload it on transfersh and get direct download link`
-
-✗  /tmpninja - `reply to a telegram file to upload it on tmpninja and get direct download link`
-
- *File Or Text To Telegraph:*
-
-✗  /tgm - `Get Telegraph Link Of Replied Media`
-
-✗  /tgt - `Get Telegraph Link of Replied Text` 
+✗ /about - About The Bot
+✗ /help - This Message
+✗ /start - Start the Bot
+✗ /generate - Start Generating Session
+✗ /cancel - Cancel the process
+✗ /restart - Cancel the process 
 
 *✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
 """
